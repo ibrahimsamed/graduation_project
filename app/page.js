f015ae35e356e9df80b5cfc8766ac6165f9aeea5
ibrahim samed <101502757+ibrahimsamed@users.noreply.github.com>
2023-05-28 01:12:12 +0300

    a

--- app/page.js
+++ app/page.js
@@ -31,7 +31,7 @@ export default function Home() {
       <div className={styles.center}>
         <Image
           className={styles.logo}
-          src="/next.svg"
+          src="/itu.svg"
           alt="Next.js Logo"
           width={180}
           height={37}
