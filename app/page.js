1f5304f0ea00607dedde421b3634b49147b69d1b
ibrahim samed <101502757+ibrahimsamed@users.noreply.github.com>
2023-05-28 00:54:27 +0300

    Update page.js

--- app/page.js
+++ app/page.js
@@ -6,7 +6,8 @@ export default function Home() {
     <main className={styles.main}>
       <div className={styles.description}>
         <p>
-          Get started by editing&nbsp;
+          ISTANBUL TECHNICAL UNIVERSITY ELECTRONICS AND COMMINICATION GRADUATED
+          PROJECT&nbsp;
           <code className={styles.code}>app/page.js</code>
         </p>
         <div>
@@ -15,7 +16,22 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            By{" "}
+            By İBRAHİM SAMED YALÇIN{" "}
+            <Image
+              src="/vercel.svg"
+              alt="Vercel Logo"
+              className={styles.vercelLogo}
+              width={100}
+              height={24}
+              priority
+            />
+          </a>
+          <a
+            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            and By ŞEMS ŞAMİL ÇAKIROĞLU{" "}
             <Image
               src="/vercel.svg"
               alt="Vercel Logo"
@@ -41,13 +57,13 @@ export default function Home() {
 
       <div className={styles.grid}>
         <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          href="https://www.memorial.com.tr/tani-ve-testler/elektromiyografi-emg-nedir"
           className={styles.card}
           target="_blank"
           rel="noopener noreferrer"
         >
           <h2>
-            Docs <span>-&gt;</span>
+            What is EMG signal <span>-&gt;</span>
           </h2>
           <p>Find in-depth information about Next.js features and API.</p>
         </a>
@@ -59,9 +75,9 @@ export default function Home() {
           rel="noopener noreferrer"
         >
           <h2>
-            Learn <span>-&gt;</span>
+            FEATURE <span>-&gt;</span>
           </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
+          <p>What will we do</p>
         </a>
 
         <a
@@ -71,9 +87,9 @@ export default function Home() {
           rel="noopener noreferrer"
         >
           <h2>
-            Templates <span>-&gt;</span>
+            progress of deployment <span>-&gt;</span>
           </h2>
-          <p>Explore the Next.js 13 playground.</p>
+          <p>we explain what we do </p>
         </a>
 
         <a
